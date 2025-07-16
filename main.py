ba28fe67988cfc562e0493aaf7aa4a7ad6d500bc
nulldata-dev <nulldata-dev@gmail.com>
2025-07-16 13:48:25 -0700

    add groundwork for multiple messages

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import sys
 from constants import *
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
     if len(sys.argv) > 1:
         response = client.models.generate_content(
-            model=AI_MODEL, contents=sys.argv[1])      
+            model=AI_MODEL, contents=messages)      
     else:
         print("WARNING: Please provide a prompt!")
         exit(code=1)
