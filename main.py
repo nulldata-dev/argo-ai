9ded5e0ce6bda1a4b0fc56c5760ffc9b69d3954d
nulldata-dev <nulldata-dev@gmail.com>
2025-07-16 13:33:12 -0700

    add user arg as input for gemini

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from constants import *
 from dotenv import load_dotenv
 from google import genai
@@ -8,12 +9,16 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model=AI_MODEL, contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    if len(sys.argv) > 1:
+        response = client.models.generate_content(
+            model=AI_MODEL, contents=sys.argv[1])      
+    else:
+        print("WARNING: Please provide a prompt!")
+        exit(code=1)
     
     print(response.text)
-
-
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
