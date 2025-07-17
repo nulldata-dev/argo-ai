58fea7788fb5ddca963d97aa5c6108b46567510a
nulldata-dev <nulldata-dev@gmail.com>
2025-07-17 16:49:53 -0700

    having issues with line 17

--- main.py
+++ main.py
@@ -9,22 +9,30 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    user_prompt = sys.argv[1]
 
-    messages = [
+    if len(sys.argv) < 1: #make sure the user inputs at least 1 input
+        print("WARNING: Please provide a prompt!")
+        exit(code=1)
+
+    user_prompt = sys.argv[1] #set the user prompt to be the first command line argument
+
+    messages = [ #turn the user prompt into a Content type for genai
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    if len(sys.argv) > 1:
-        response = client.models.generate_content(
-            model=AI_MODEL, contents=messages)      
-    else:
-        print("WARNING: Please provide a prompt!")
-        exit(code=1)
+    response = client.models.generate_content( #make a call to genai to generate a response
+        model=AI_MODEL, contents=messages)      
+        
     
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if len(sys.argv) > 1:
+        for argument in sys.argv[1:]: #for every argument after the first
+            if argument == "--verbose": #check if the user 
+                print(f"User prompt: {user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
