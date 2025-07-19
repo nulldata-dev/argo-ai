3318d9d6d4306d31dbe3c742e11c2929d1d64e6d
nulldata-dev <nulldata-dev@gmail.com>
2025-07-18 21:49:38 -0700

    working on get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,22 +5,22 @@ def get_files_info(working_directory, directory="."):
     working_directory_abs = os.path.abspath(working_directory)
     target_dir_abs = os.path.abspath(path_to_directory)
 
-    if not target_dir_abs.startswith(working_directory_abs): #make sure the 
+    if not target_dir_abs.startswith(working_directory_abs): #make sure the target dir is in the working dir
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(path_to_directory): #check if directory is a directory
         return f'Error: "{directory}" is not a directory'
     
-    directory_contents = os.listdir(directory)
+    directory_contents = os.listdir(path_to_directory)
     dir_contents_string = "" #will become the output
 
     for file in directory_contents: #loop through all files in the working dir
-        path_to_file = os.path.join(working_directory, file)
+        path_to_file = os.path.join(path_to_directory, file)
         print(f"path_to_file: {path_to_file}")
         file_size = os.path.getsize(path_to_file)
         file_is_dir = os.path.isdir(path_to_file)
-        dir_contents_string += f"- {file}: files_size={file_size}, is_dir={file_is_dir}\n"
+        dir_contents_string += f"- {file}: file_size={file_size}, is_dir={file_is_dir}\n"
     
-    print(dir_contents_string) #test
+    return dir_contents_string #output the string
     
 
-get_files_info("~/workspace/github.com/nulldata-dev/argo-ai")
+print(get_files_info("~/workspace/github.com/nulldata-dev/argo-ai"))
