a3e1992563a7b3549c0e21613d325dd862d7c607
nulldata-dev <nulldata-dev@gmail.com>
2026-04-03 15:23:45 -0700

    get_files_info.py v1 complete

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,23 +1,42 @@
 import os
 
-def get_files_info(working_directory, directory="."):
-    path_to_directory = os.path.join(working_directory, directory)
-    working_directory_abs = os.path.abspath(working_directory)
-    target_dir_abs = os.path.abspath(path_to_directory)
+def get_files_info(working_dir, directory="."):
+    try:
+        abs_working_dir = os.path.abspath(working_dir) #absolute path from relative path, based off the user set working directory
+    except:
+        return f'Error: Could not get abspath of "{working_dir}"'
+    
+    target_dir = os.path.normpath(os.path.join(abs_working_dir, directory)) #normalized, joined path to the target directory
 
-    if not target_dir_abs.startswith(working_directory_abs): #make sure the target dir is in the working dir
+    valid_target_dir = os.path.commonpath([abs_working_dir, target_dir]) == abs_working_dir #check if the target dir is within the working dir
+    valid_input_dir = os.path.isdir(target_dir) #check if the target dir is a valid directory
+
+    if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(path_to_directory): #check if directory is a directory
+    if not valid_input_dir:
         return f'Error: "{directory}" is not a directory'
     
-    directory_contents = os.listdir(path_to_directory)
-    dir_contents_string = "" #will become the output
-
-    for file in directory_contents: #loop through all files in the working dir
-        path_to_file = os.path.join(path_to_directory, file)
-        print(f"path_to_file: {path_to_file}")
-        file_size = os.path.getsize(path_to_file)
-        file_is_dir = os.path.isdir(path_to_file)
-        dir_contents_string += f"- {file}: file_size={file_size}, is_dir={file_is_dir}\n"
+    try:
+        items_in_dir = os.listdir(target_dir)
+    except:
+        return f'Error: os.listdir failed on "{target_dir}"'
     
-    return dir_contents_string #output the string
+    files_info = []
+
+    for item in items_in_dir:
+        try:
+            item_path = os.path.normpath(os.path.join(target_dir, item))
+        except:
+            return f'Error: item_path failed on "{item}"'
+        try:
+            item_size = os.path.getsize(item_path)
+        except:
+            return f'Error: item_size failed on "{item}"'
+        try:
+            item_is_dir = os.path.isdir(item_path)
+        except:
+            return f'Error: item_is_dir failed on "{item}"'
+        
+        files_info.append(f'{item}: file_size={item_size} bytes, is_dir={item_is_dir}')
+    
+    return "\n".join(files_info)
